b2f3a36665d4f527701aff32902731da3c6165ec
isa-id <juanse.giraldo07@gmail.com>
2025-08-31 23:46:13 -0500

    101s V0.1

--- project/commands/utility/ping.js
+++ project/commands/utility/ping.js
@@ -1,8 +1,10 @@
-const { SlashcommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashcommandBuilder().name('ping').setDescription('Replies with Pong!'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
 };
